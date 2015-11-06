a37f83146024d2ca613e69b25bace47f6f63b5fe
mplarkin95 <mplarkin95@gmail.com>
2015-11-05 21:38:52 -0500

    added gausian Mixture class and updated functionality of gausian standard 2-d curve

--- gausian0.py
+++ gausian0.py
@@ -1,6 +1,7 @@
 import math as M
 import numpy as nu
 
+#single gausian curve
 class gaus_std(object):
 	"""Make a gausian std distro"""
 	def __init__(self, x_mean,y_mean,var_x,var_y):	
@@ -9,6 +10,8 @@ class gaus_std(object):
 		self.x_v = var_x
 		self.y_v = var_y
 
+	def __str__(self):
+		return "x mean: "+ str(self.x_m)+"\ny mean: " + str(self.y_m)+"\nx variance: "+ str(self.x_v)+"\ny variance: "+ str(self.y_v) 
 	def calculate(self,point):
 		x_m = self.x_m
 		x_v = self.x_v
@@ -16,19 +19,58 @@ class gaus_std(object):
 		y_v = self.y_v
 		x=point[0]
 		y=point[1]
-		return final = 
-			(1/(2*M.pi*M.sqrt(x_v)*M.sqrt(y_v)))*M.exp(-.5*
-				(
-					(M.pow((x-x_m),2)/x_v)+
-					(M.pow((y-y_m),2)/y_v)
-				)
+		return(1/(2*M.pi*M.sqrt(x_v)*M.sqrt(y_v)))*M.exp(-.5*((M.pow((x-x_m),2)/x_v)+(M.pow((y-y_m),2)/y_v)))
+
+	def x_mean_change(self,n):
+		self.x_m = n
+
+	def y_mean_change(self,n):
+		self.y_m = n
+
+	def x_var_change(self,n):
+		self.x_v = n
+
+	def y_var_change(self,n):
+		self.y_v = n
 
 
+#gausian Mixture model
+class gaus_mix(object):
+	"""Create a mixture model with set number of components"""
+	def __init__(self,num_comp,*components):
+		if(num_comp != len(components)): raise ValueError('enter correct # of components')
+		self.n_c = num_comp
+		self.weights = {}
+		self.num = []
+		for c in components:
+			self.weights[c]=1
+			self.num.append(c)
+
+	def __str__(self):
+		string = ''
+		i = 0
+		for k in self.num:
+			string += "Component "+ str(i)+ '\n'+str(k)+ "\nWeight: "+ str(self.weights[k])+ "\n\n"
+			i+=1
+		return string
+	
+	def component(self,n):
+		return self.num[n]
+
+	def change_weight(self,component,nWeight):
+		self.weights[self.num[component]]=nWeight
 
 
+#########################
+#		TESTING
+#########################
+a = gaus_std(6,5,1,.8)
+b = gaus_std(7,12,.6,1.2)
+c = gaus_std(9,11,.9,2.1)
+
+g= gaus_mix(3,a,b,c)
+print g
+g.change_weight(1,2)
+print g
 
 
-
-
-
-		
