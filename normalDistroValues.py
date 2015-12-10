88dec57e2c366e96d6aaa0be57a67296dc4b1364
jonathancyr <jjcyr527@gmail.com>
2015-12-10 00:33:44 -0500

    Created normalDistroValues.py

---
+++ normalDistroValues.py
@@ -0,0 +1,44 @@
+import numpy as nu
+import math
+
+
+class NormDistVal(object):
+    """
+    A class for 2-d data generation with normally distributed values
+    """
+
+    def __init__(self, x_mean, y_mean, x_var, y_var, amount_points):
+        self.x_mean = x_mean
+        self.y_mean = y_mean
+        self.x_var = x_var
+        self.y_var = y_var
+        self.x_dev = math.sqrt(x_var)
+        self.y_dev = math.sqrt(y_var)
+        self.points = self.calc_norm_dist_val(amount_points)
+        self.number_points = amount_points
+
+    def __str__(self):
+        return "x mean: " + str(self.x_m) + "\ny mean: " + str(self.y_m) + "\nx variance: " + str(
+            self.x_v) + "\ny variance: " + str(self.y_v) + "\nx standard deviation: " + str(
+            self.x_dev) + "\ny stadard deviation: " + str(self.y_dev)
+
+    def calc_norm_dist_val(self, amount_points):
+        x = nu.random.normal(self.x_mean, self.x_dev, amount_points)
+        y = nu.random.normal(self.y_mean, self.y_dev, amount_points)
+        points_list = []
+        for i in range(0, amount_points):
+            point = [0,1]
+            for j in x:
+                point[0] = j
+            for k in y:
+                point[1] = k
+            points_list.append(point)
+        return points_list
+
+    def get_points(self):
+        return self.points
+
+    def get_number_points(self):
+        return self.number_points
+
+
